f047b9e17184d1864b79415b86e34150cf13c1a3
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-05-07 17:37:34 +0300

    Create Django project

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns.append(path("admin/", admin.site.urls))
